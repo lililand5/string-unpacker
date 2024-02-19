ca2666f319befa559f5cf0f98ada24b21830317e
lililand5 <tor-ver@mail.ru>
2024-02-19 15:21:42 +0300

    simple-unpacker

--- simple-unpacker/simple-unpacker.go
+++ simple-unpacker/simple-unpacker.go
@@ -7,118 +7,109 @@ import (
 	"strings"
 )
 
-// разбиваем строку на слайс символов
-// Возвращает ошибку, если после экранирования стоит цифра,
-// Возвращает ошибку, если встречается число,
-// Возвращает ошибку, если первый символ это цифра
-func SplitStringIntoSlice(input string) ([]string, error) {
-	var result []string // слайс в наполняем символами из строки input
-	var escapeNext bool // нужно ли добавлять слэш след. символу
+// разбиваем строку на слайс рун и делаем проверки
+func SplitStringIntoSlice(input string) ([]rune, error) {
+	var result []rune
+	var escapeNext bool
 
-	for i := 0; i < len(input); i++ {
-		if i == 0 && input[i] >= '0' && input[i] <= '9' {
-			return nil, errors.New("первый символ в строке не может быть цифрой")
-		}
-
-		if escapeNext {
-			if input[i] >= '0' && input[i] <= '9' {
-				return nil, errors.New("нельзя использовать цифру после экранирования")
+	for _, char := range input {
+		// Если текущий символ цифра, то делаем проверки:
+		if char >= '0' && char <= '9' {
+			if len(result) == 0 {
+				return nil, errors.New("первый символ не может быть цифрой")
 			}
-			result = append(result, "\\"+string(input[i]))
-			escapeNext = false
-			continue
+			if escapeNext {
+				return nil, errors.New("цифра не может стоять после слэша")
+			}
+			// если текущая цифра не первая и если предыдущий символ не слэш,
+			// то проверяем что предыдущий символ не цифра
+			if len(result) > 0 && !escapeNext {
+				prevChar := result[len(result)-1]
+
+				if prevChar >= '0' && prevChar <= '9' {
+					return nil, errors.New("строка не может содержать последовательность цифр")
+				}
+			}
 		}
 
-		// Если escapeNext - true, значит в предыдущей итерации(в предыдущем символе)
-		// обнаружен символ слэш
-		// тогда нельзя чтобы текущий символ тоже был слэш
-		if input[i] == '\\' {
+		if char == '\\' {
 			if escapeNext {
 				return nil, errors.New("нельзя использовать два подряд идущих символа экранирования")
 			}
-			// Если escapeNext - false, но следующий символ является слэшем, то тоже ошибка
-			if i+1 < len(input) && input[i+1] == '\\' {
-				return nil, errors.New("нельзя использовать два подряд идущих символа экранирования")
-			}
-			// Если текущий символ сэш, и следующий символ не слэш и не цифра,
-			// значит обнуляем escapeNext
-			escapeNext = true
+			escapeNext = !escapeNext
 			continue
 		}
 
-		if input[i] >= '0' && input[i] <= '9' {
-			if i > 0 && input[i-1] >= '0' && input[i-1] <= '9' {
-				return nil, errors.New("строка не может содержать последовательность цифр")
-			}
+		if escapeNext {
+			result = append(result, '\\', char) // Добавляем слэш вместе с символом
+			escapeNext = false
+		} else {
+			result = append(result, char)
 		}
-
-		result = append(result, string(input[i]))
 	}
 
 	return result, nil
 }
 
-// преобразование слайса в двумерный слайс,
-// где каждый подслайс группирует символ и количество его повторений
-// считаем количество повторений для каждого символа
-func ConvertSlice(slice []string) ([][]string, error) {
-	var result [][]string
-
-	for i := 0; i < len(slice); i++ {
-		char := slice[i]
-		countChar := "1" // По умолчанию каждый символ встречается один раз
-
-		// Если след.элемент - количество повторений текущего символа,
-		// то countChar присваивается количество,
-		// и пропускаем следующий элемент, так как он уже обработан
-		if i+1 < len(slice) && slice[i+1][0] >= '0' && slice[i+1][0] <= '9' {
-			countChar = slice[i+1]
-			i++
-		}
-
-		// Преобразуем строку с количеством в число
-		count, err := strconv.Atoi(countChar)
-		if err != nil {
-			fmt.Println("Ошибка при преобразовании строки в число:", err)
-			continue
-		}
-
-		if count == 0 {
-			continue // Пропускаем символы с количеством 0
-		}
-
-		// Повторяем символ count раз
-		transformedChar := strings.Repeat(char, count)
-		result = append(result, []string{transformedChar, countChar})
-	}
-
-	return result, nil
-}
-
-// Преобразуем двумерный слайс в строку
-func SliceToString(slice [][]string) string {
+// Преобразование слайса rune в строку с учетом повторений символов
+func ConvertSlice(slice []rune) (string, error) {
+	// fmt.Printf("Slice content: %q\n", slice)
 	var stringBuilder strings.Builder
 
-	for _, item := range slice {
-		stringBuilder.WriteString(item[0])
+	i := 0
+	for i < len(slice) {
+		char := slice[i]
+		var count int = 1
+
+		// Проверяем, что текущий символ не последний и что он - слэш
+		escapeChar := i+1 < len(slice) && char == '\\'
+
+		if escapeChar {
+			// Если да, то помечаем след символ, и пропускаем эти два символа
+			nextChar := slice[i+1]
+			i += 2
+
+			// Проверяем, является ли следующий после экранированной последовательности цифрой
+			// если является, то count меняем на новую цифру
+			// если нет, то count останется 1
+			if i < len(slice) && slice[i] >= '0' && slice[i] <= '9' {
+				var err error
+				count, err = strconv.Atoi(string(slice[i]))
+				if err != nil {
+					return "", err
+				}
+				i++
+			}
+
+			// Добавляем экранированную последовательность в соответствии с её количеством
+			stringBuilder.WriteString(strings.Repeat(string(char)+string(nextChar), count))
+		} else { // либо если текущий символ не слэш, то просто добавляем этот символ
+			i++
+			// Проверяем, является ли следующий символ цифрой
+			if i < len(slice) && slice[i] >= '0' && slice[i] <= '9' {
+				count, _ = strconv.Atoi(string(slice[i]))
+				i++
+			}
+
+			// Добавляем символ в соответствии с его количеством
+			stringBuilder.WriteString(strings.Repeat(string(char), count))
+		}
 	}
 
-	return stringBuilder.String()
+	return stringBuilder.String(), nil
 }
 
 // Результирующая функция
 func ProcessInputString(input string) (string, error) {
 	slice, err := SplitStringIntoSlice(input)
 	if err != nil {
-		return "", err // Ошибка на этапе разбивки строки на символы
+		return "", err
 	}
 
-	convertedSlice, err := ConvertSlice(slice)
+	transformedString, err := ConvertSlice(slice)
 	if err != nil {
-		return "", err // Ошибка на этапе подсчета символов
+		return "", err
 	}
-
-	transformedString := SliceToString(convertedSlice)
 
 	return transformedString, nil
 }
@@ -126,14 +117,14 @@ func ProcessInputString(input string) (string, error) {
 func main() {
 	// Тестовые примеры
 	testCases := []string{
-		`a4bc2d5e`,
-		`abcd`,
-		`3abc`,
-		`45`,
-		`aaa10b`,
-		`aaa0b`,
-		``,
-		`d\n5abc`,
+		`a4bc2d5e`, // => "aaaabccddddde"
+		`abcd`,     // => "abcd"
+		`3abc`,     // Ошибка
+		`45`,       // Ошибка
+		`aaa10b`,   // Ошибка
+		`aaa0b`,    // => "aab"
+		``,         //  => ""
+		`d\n5abc`,  // => "d\n\n\n\n\nabc"
 	}
 
 	for _, testCase := range testCases {
